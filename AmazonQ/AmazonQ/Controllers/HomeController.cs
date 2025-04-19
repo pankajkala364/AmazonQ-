3f3c6991389ef80b4d579355fb7b0b603694b550
NoReply Amazon <no-reply@amazon.com>
2025-04-19 12:19:03 +0000

    Initial commit of modernized application

--- AmazonQ/AmazonQ/Controllers/HomeController.cs
+++ AmazonQ/AmazonQ/Controllers/HomeController.cs
@@ -1,8 +1,9 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
-using System.Web.Mvc;
+using Microsoft.AspNetCore.Mvc;
+
 
 namespace AmazonQ.Controllers
 {
